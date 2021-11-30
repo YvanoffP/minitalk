495d3fd5a50b2cd59ee26bf97b5b7792e6d4f7da
YvanoffP <petruzzi.yvan@gmail.com>
2021-11-30 16:23:50 +0100

    ok mdr

--- client.c
+++ client.c
@@ -1,15 +1,38 @@
 #include "minitalk.h"
 
+void	send_char_by_bit(char c, int pid)
+{
+	int	i;
+
+	i = -1;
+	while (++i < 8)
+	{
+		if (c & 0x01)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		c = c >> 1;
+		usleep(100);
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	int	pid;
+	int	i;
 	char *str_send;
 
+	i = 0;
 	if (argc != 3)
-		printf("ERROR\n");
+	{
+		write(1, "ERROR\n", 6);
+		exit(1);
+	}
 	pid = ft_atoi(argv[1]);
 	str_send = argv[2];
-	printf("PID is %i\nSTR to send is %s\n", pid, str_send);
-
-	return (0);
+	while (str_send[i] != '\0')
+	{
+		send_char_by_bit(str_send[i], pid);
+		i++;
+	}
 }
--- server.c
+++ server.c
@@ -1,10 +1,41 @@
 #include "minitalk.h"
 
+int     calcul_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	else if (power == 0)
+		return (1);
+	else
+		return (nb * calcul_power(nb, power - 1));
+}
+
+void	handler(int signum)
+{
+	static int	current_bit = 0;
+	static int	char_value = 0;
+
+	if (signum == SIGUSR2)
+		char_value += calcul_power(2, current_bit);
+	if (current_bit == 7)
+	{
+		ft_putchar_fd(char_value, 0);
+		current_bit = 0;
+		char_value = 0;
+		return ;
+	}
+	current_bit++;
+}
+
 int	main(void)
 {
 	int	pid;
 
 	pid = getpid();
-	printf("%i\n", pid);
-	sleep(10);
+	ft_putnbr_fd(pid, STDIN_FILENO);
+	ft_putchar_fd('\n', STDIN_FILENO);
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	while (1)
+		usleep(100);
 }
